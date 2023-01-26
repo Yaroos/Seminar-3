30c8337bcc05870746d3e4328fbe170144d1fb08
Yaroslav <rotochenkov@icloud.com>
2023-01-26 13:15:44 +0300

    First replace 2/3 Task.

---
+++ Task_19_Pallindrom/Program.cs
@@ -0,0 +1,39 @@
+﻿string message = "Введите 5 значное число:";
+int GetNumber(string message)
+{
+    int resultNumber = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out resultNumber) && resultNumber > 9999 && resultNumber < 100000)
+        {
+        break;
+        }
+        else
+        {
+            Console.WriteLine("ВВели не число или не коректное число. Повторите ввод.");
+        }
+    }
+    return resultNumber;
+}
+
+int forWorkMethod = GetNumber(message);
+
+int WorkMethod(int forWorkMethod)
+{
+    if (forWorkMethod / 10000 == forWorkMethod % 10 && forWorkMethod/1000%10 == forWorkMethod/10%10)
+    {
+        Console.WriteLine("Это палиндром");
+    }
+    else 
+    {
+        Console.WriteLine("Это не палиндром");
+    }
+    return forWorkMethod;
+}
+int Checkout = WorkMethod(forWorkMethod);
+
+
+
---
+++ Task_23_Cube/Program.cs
@@ -0,0 +1,27 @@
+﻿string message = "Введите число, до которого хотите посмотреть таблицу кубов:";
+int GetNumber(string message)
+{
+    int resultNumber = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out resultNumber))
+        {
+        break;
+        }
+        else
+        {
+            Console.WriteLine("ВВели не число или не коректное число. Повторите ввод.");
+        }
+    }
+    return resultNumber;
+}
+
+int number = GetNumber(message);
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(i*i*i + ","); 
+}
